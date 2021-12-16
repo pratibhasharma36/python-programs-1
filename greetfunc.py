7a6b4b5058e0c9c54e3ad8d6e10a2465470d23a2
Pratibha <pratibha4236@gmail.com>
2021-12-16 23:16:15 +0530

    Added a function

---
+++ greetfunc.py
@@ -0,0 +1,20 @@
+def greet(name):
+	print("Good Day, "+ name)
+
+greet("Pratibha")
+
+#User defined function
+
+def greet(name = "Stranger"):
+	print("Good Day, "+ name)
+
+
+greet()
+
+#sum program
+
+def mysum(num1, num2):
+	return num1 + num2
+mysum(3,5)
+print(mysum(3,5))
+
